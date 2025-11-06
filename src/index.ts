94982dd212fc5124984e2e500b45b99424038ed2
Raine <randomrandom.epic.epic@gmail.com>
2025-11-06 10:54:17 -0500

    feat: update exports

--- src/index.ts
+++ src/index.ts
@@ -15,7 +15,30 @@ declare global {
 		LayoutOrder?: number;
 	}
 
+	type DestroyableBind<a extends unknown[]> = (callback: (...args: a) => void) => () => void;
+
 	export type InputStyle = "basic" | "main" | "invisible" | "danger";
 
 	export type ColorModifier = "Default" | "Selected" | "Hovered" | "Pressed" | "Disabled";
 }
+
+// Hooks
+export * from "./hooks/plugin/usePlugin";
+export * from "./hooks/useStudioTheme";
+export * from "./hooks/useTheme";
+
+// Contexts
+//export * from "./contexts/plugin/MenuContext"
+//export * from "./contexts/plugin/PluginContext"
+//export * from "./contexts/plugin/ToolbarContext"
+//export * from "./contexts/plugin/WidgetContext"
+//export * from "./contexts/ThemeContext"
+
+// Components
+export * from "./components/plugin/PluginProvider";
+export * from "./components/plugin/PluginWidget";
+
+export * from "./components/Background";
+export * from "./components/Button";
+export * from "./components/Icon";
+export * from "./components/TabContainer";
